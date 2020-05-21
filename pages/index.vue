8d8ebf5cae15e9a3a3ce896d10cf7df9336c4fee
ZZJ <zhaozjseu@163.com>
2020-05-21 18:11:10 +0800

    修改名称

--- pages/index.vue
+++ pages/index.vue
@@ -243,8 +243,8 @@ const rightItem = {
         "https://seicwxbz.seu.edu.cn/cas-we-can/login?goto=http://zwys.seu.edu.cn:8080/ddshower/f/wechat/user/showerIndex"
     },
     {
-      key: "外出审批（测试）",
-      name: "外出审批（测试）",
+      key: "外出审批",
+      name: "疫情期间外出审批系统",
       access: ["1","3"],
       icon: require("~/assets/right-icon/wcsp.svg"),
       url:
