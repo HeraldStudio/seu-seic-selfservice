515dbb52da0ed99f5b7701f79f835a69d031eb42
R_shrug <656598766@qq.com>
2020-08-18 02:13:06 +0800

    修改logout

--- pages/index.vue
+++ pages/index.vue
@@ -61,7 +61,7 @@
     <img
       src="../assets/logout_1.svg"
       class="float-logo"
-      style="width: 32px;height: 32px;"
+      style="width: 64px;height: 64px;"
       @click="logout"
     />
     <!-- 语音早安 -->
