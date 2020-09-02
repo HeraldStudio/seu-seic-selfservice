adaf40355fc522a293aa0afd3b0d767f986987f6
R-shrug <213183580@seu.edu.cn>
2020-09-02 20:46:29 +0800

    更新网站群

--- pages/index.vue
+++ pages/index.vue
@@ -231,12 +231,12 @@ const rightItem = {
     },
   ],
   个人服务: [
-    {
+    { 
       key: "服务指南",
       name: "服务指南",
       access: "all",
       icon: require("~/assets/right-icon/fwzn.svg"),
-      url: "https://nic.seu.edu.cn/wxfwlc/",
+      url: "http://nic.seu.edu.cn/bxs/zxgw.htm",
     },
     {
       key: "ZOOM云会议",
