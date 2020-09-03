c6bdac2839f27133828de757ef7754771cce4df8
R-shrug <213183580@seu.edu.cn>
2020-09-03 15:58:40 +0800

    试探

--- pages/index.vue
+++ pages/index.vue
@@ -572,7 +572,7 @@ export default {
           // redirect('http://127.0.0.1/')
         }
       } else {
-        console.log(req);
+        console.log(req.path);
         redirect(
           `https://seicwxbz.seu.edu.cn/cas-we-can/login?goto=${encodeURIComponent(
             "https://seicwxbz.seu.edu.cn/self-service"
