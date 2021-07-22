209f2e3f7a73afa16977278b5c847db1df5214fb
wangjialei <Jialei_w@163.com>
2021-07-22 14:58:19 +0800

    添加两位老师的云会议申请权限

--- pages/index.vue
+++ pages/index.vue
@@ -297,7 +297,7 @@ const rightItem = {
     {
       key: "云会议申请",
       name: "云会议申请",
-      access: ["213181019","320002594","213171780"],
+      access: ["213181019","320002594","213171780", "101005090", "103008527"],
       icon: require("~/assets/right-icon/yhysq.svg"),
       url: "http://meeting.seu.edu.cn/h5/",
     },
